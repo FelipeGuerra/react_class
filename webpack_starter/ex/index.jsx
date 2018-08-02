a7353984056851a2e717e959c0235dc238cac333
lefipe <lefipe.guerra@gmail.com>
2018-08-02 19:05:09 +0100

    [update] component with handle change

--- webpack_starter/ex/index.jsx
+++ webpack_starter/ex/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ClassComponent from './classComponent'
+import Field from './field'
 
 ReactDOM.render(
-    <ClassComponent label='Contador' initialValue={10} value={10} />
+    // <ClassComponent label='Contador' initialValue={10} value={10} />
+    
+    <Field initialValue='Teste' />
 , document.getElementById('app'))
