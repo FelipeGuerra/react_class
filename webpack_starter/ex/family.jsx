71f3326d8d078bac6678de60072745d573b7ea7b
lefipe <lefipe.guerra@gmail.com>
2018-08-02 15:43:03 +0100

    [update] component with properties

--- webpack_starter/ex/family.jsx
+++ webpack_starter/ex/family.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Member from './member'
+// import Member from './member'
+import {childrenWithProps} from '../utils/reactUtils'
 
 export default props => (
     <div>
-        <Member name="Name1" lastName="LastName1" />
-        <Member name="Name2" lastName="LastName2" />
-        <Member name="Name3" lastName="LastName3" />
-        <Member name="Name4" lastName="LastName4" />
+        <h1>Family</h1>
+        {childrenWithProps(props.children, props)}
     </div>
 )
--- webpack_starter/ex/index.jsx
+++ webpack_starter/ex/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Family  from './family'
+import Member  from './member'
 
 ReactDOM.render(
     <div>
-        <Family />
+        <Family lastName="familyName">
+            <Member name="Name1" />
+            <Member name="Name2" />
+            <Member name="Name3" />
+        </Family>
     </div>
 , document.getElementById('app'))
